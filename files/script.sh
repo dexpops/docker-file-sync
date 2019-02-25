b5db339dcd108dcfa02f57f3dfd9ac9fa9e3113e
Karsten Kaj Jakobsen <kj@patientsky.com>
2019-02-25 09:42:09 +0100

    testing nomad

--- files/script.sh
+++ files/script.sh
@@ -1,52 +1,34 @@
 #!/bin/bash
 
-if [ -z $METHOD ]
+if [ -z $URL ]
 then
-  METHOD="wget"
-fi
-
-if [ $METHOD == "wget" ]
-then
-
-  if [ -z $WGET_URL ]
-  then
-    WGET_URL="http://utxosets.blob.core.windows.net/public/utxo-snapshot-bitcoin-mainnet-551636.tar"
-  fi
-
-  if [ -z $WGET_DIR ]
-  then
-    WGET_DIR="/utxo"
-  fi
-
-  if [ -z $WGET_FILE_NAME ]
-  then
-    WGET_FILE_NAME="utxo-snapshot.tar"
-  fi
-
-  if [ -z $WGET_FISNIHED_MARKER ]
-  then
-    WGET_FISNIHED_MARKER=".finished_utxo"
-  fi
-
-  mkdir -p $WGET_DIR
-
-  # Check if marker exists
-  if [ ! -f $WGET_DIR/$WGET_FISNIHED_MARKER ];
-  then
-    wget $WGET_URL -O "$WGET_DIR/$WGET_FILE_NAME"
-    touch "$WGET_DIR/$WGET_FISNIHED_MARKER"
-  fi
-
-else
-
-  echo "$METHOD is not supported"
+  echo "Error, no URL supplied"
   exit 1
-
 fi
 
-while true
-do
-  echo "Done syncing, created $WGET_DIR/$WGET_FISNIHED_MARKER"
-  stat "$WGET_DIR/$WGET_FISNIHED_MARKER"
-  sleep 10
-done
+if [ -z $FILENAME ]
+then
+  echo "Error, no FILENAME supplied"
+  exit 1
+fi
+
+if [ -z $DOWNLOAD_DIR ]
+then
+  DOWNLOAD_DIR="/app"
+fi
+
+echo "Making dir $DOWNLOAD_DIR"
+mkdir -p $DOWNLOAD_DIR
+
+TEMP_FILENAME_SUFFIX="_temp_downloading"
+TEMP_FILENAME="$FILENAME$TEMP_FILENAME_SUFFIX"
+
+# Check if marker exists
+if [ ! -f $DOWNLOAD_DIR/$FILENAME ];
+then
+  wget $URL -O $DOWNLOAD_DIR/$TEMP_FILENAME
+  mv $DOWNLOAD_DIR/$TEMP_FILENAME $DOWNLOAD_DIR/$FILENAME
+fi
+
+echo "$(date): Done syncing, created $DOWNLOAD_DIR/$FILENAME"
+exit 0
--- makefile
+++ makefile
@@ -1,11 +1,11 @@
 all: build run
 
 run:
-	docker stop file_sync
-	docker run --rm -it --name file_sync \
-	-v file_sync-volume:/utxo \
-	-e WGET_FISNIHED_MARKER=".finshed_test_marker" \
-	-e WGET_URL="http://utxosets.blob.core.windows.net/public/utxo-snapshot-bitcoin-mainnet-551636.tar" \
+	docker stop file_sync; \
+	docker run --rm --name file_sync \
+	-v /Users/kj/temp_data:/app \
+	-e URL="http://utxosets.blob.core.windows.net/public/utxo-snapshot-bitcoin-mainnet-551636.tar" \
+	-e FILENAME="utxo-snapshot-bitcoin.tar" \
 	dexpops/docker-file-sync:latest
 
 stop:
@@ -14,7 +14,6 @@ stop:
 prune:
 	docker stop file_sync
 	docker rm file_sync
-	docker volume rm file_sync-volume
 
 build:
 	docker build -t dexpops/docker-file-sync:latest .
